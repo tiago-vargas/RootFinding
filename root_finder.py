8ec50734e70a584f89bc212e53c44920acce50f9
Tiago Vargas <tiago.ce10@outlook.com>
2022-10-12 22:43:59 -0300

    Feat: get root for low precisions

--- root_finder.py
+++ root_finder.py
@@ -2,4 +2,10 @@ from typing import Callable
 
 
 def find_root(func: Callable[[float], float], interval: tuple[float, float], precision: float) -> float:
-	raise Exception
+	(a, b) = interval
+
+	if func(a) * func(b) >= 0:
+		raise Exception('No root guaranteed to exist in the interval ' + str(interval))
+
+	if precision >= b - a:
+		return (a + b) / 2
--- test_root_finder.py
+++ test_root_finder.py
@@ -3,30 +3,20 @@ from root_finder import *
 
 
 class TestBissectionMethod:
-	# def test_linear_function_with_high_precision(self):
-	# 	function = lambda x: 2*x + 1
-
-	# 	epsilon = 1e-5
-
-	# 	root = find_root(function, (-2, 0), epsilon)
-
-	# 	assert -1 - epsilon <= root <= -1 + epsilon
-
-
-	# def test_linear_function_with_low_precision(self):
-	# 	function = lambda x: 2*x + 1
-
-	# 	epsilon = 4
-
-	# 	root = find_root(function, (-2, 0), epsilon)
-
-	# 	assert -1 - epsilon <= root <= -1 + epsilon
-
-
 	def test_if_root_is_not_guaranteed_to_exist_in_interval(self):
 		function = lambda x: x
 		interval = (1, 2)
-		epsilon = 1e-5
+		precision = 1e-5
 
 		with pytest.raises(Exception):
-			root = find_root(function, interval, epsilon)
+			root = find_root(function, interval, precision)
+
+
+	def test_finding_root_of_linear_function_with_low_precision(self):
+		function = lambda x: 2*x + 1
+		interval = (-1, 0)
+		precision = 4
+
+		root = find_root(function, interval, precision)
+
+		assert -0.5 - precision < root < -0.5 + precision
