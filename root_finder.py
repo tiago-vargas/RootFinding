400a9746876eca3cc05e9fca77fdb6d81cc7e932
Tiago Vargas <tiago.ce10@outlook.com>
2022-10-12 22:19:34 -0300

    Feat: raise exception when no root is guaranteed
    Raise exception when no root is guaranteed to exist in the interval

--- root_finder.py
+++ root_finder.py
@@ -1,1 +1,5 @@
+from typing import Callable
 
+
+def find_root(func: Callable[[float], float], interval: tuple[float, float], precision: float) -> float:
+	raise Exception
--- test_root_finder.py
+++ test_root_finder.py
@@ -1,1 +1,12 @@
+import pytest
 from root_finder import *
+
+
+class TestBissectionMethod:
+	def test_if_root_is_not_guaranteed_to_exist_in_interval(self):
+		function = lambda x: x
+		interval = (1, 2)
+		epsilon = 1e-5
+
+		with pytest.raises(Exception):
+			root = find_root(function, interval, epsilon)
